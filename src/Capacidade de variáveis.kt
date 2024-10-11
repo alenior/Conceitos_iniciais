cb4af59e7c95b201b1674f0b7077b0a98338c426
alenior <alenior@gmail.com>
2024-10-10 21:16:58 -0300

    Capítulo 2

---
+++ src/Capacidade de variáveis.kt
@@ -0,0 +1,6 @@
+fun main() {
+    println("ULong Máximo: ${ULong.MAX_VALUE} ULong Mínimo: ${ULong.MIN_VALUE}")
+    println("UInt Máximo: ${UInt.MAX_VALUE} UInt Mínimo: ${UInt.MIN_VALUE}")
+    println("UShort Máximo: ${UShort.MAX_VALUE} UShort Mínimo: ${UShort.MIN_VALUE}")
+    println("Long Máximo: ${Long.MAX_VALUE} Long Mínimo: ${Long.MIN_VALUE}")
+}
---
+++ src/Tipos de variáveis.kt
@@ -0,0 +1,31 @@
+fun main() {
+    var x: Int = 18 // Variável mutável
+    val y: Int = 0// Variável imutável
+    var a: Float = 3.123456f
+    var b: Double = 1.1111111
+    var c: Long = 1234567890
+    var d: Char = 'F'
+    var e: String = "Felipe"
+    var f: Short = 100
+    var g: Byte = 0b11
+    var h: Byte = 0x10
+    var i: UInt = 12u // Unsigned
+
+    println(x)
+    println(y)
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+    println(i)
+}
+/*
+* Comentário
+* de
+* múltiplas
+* linhas
+* */
